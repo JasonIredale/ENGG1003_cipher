9db0b29c8014130c0ed90264890e8dbcd19010b9
Jason Iredale <jason.iredale@uon.com>
2019-04-02 00:57:29 +0000

    baefgf lab 6

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char a= '?'
+    printf("who is the coolest cat in the ENGG1003 course?");
+    scanf("%d", a);
+    if(a=a) {
+        printf(Jason Iredale!!);
+    }
+    else{
+        ("No. It's Jason Iredale!")
+    }
+    return 0;
+}

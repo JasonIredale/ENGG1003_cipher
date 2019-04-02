82d9145542a44e47093621340c6a7cb176323ff7
Jason Iredale <jason.iredale@uon.com>
2019-04-02 00:49:02 +0000

    commit(0)-for lab 6

---
+++ main.c.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a
+    printf("who is the coolest cat in the ENGG1003 course?");
+    scanf("%d", a);
+    if(a=1) {
+        printf(Jason Iredale!!);
+    }
+    else{
+        ("No. It's Jason Iredale!")
+    }
+    return 0;
+}

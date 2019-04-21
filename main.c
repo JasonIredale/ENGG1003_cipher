b88d0424235a12f574edcb8a474ffb89a97a30ca
Jason Iredale <jason.iredale@uon.com>
2019-04-21 12:29:57 +0000

    I don't know how to use scanf in Che so what i'm going to do is add the key in the 1st space of the Caesar input file and have the program both encrypt it and decrypt it in different files by closing copies of the program in braces. I feel like this is a short solution for bigger problems down the line but so far it is the only thing i can come up with that works

--- main.c
+++ main.c
@@ -1,40 +1,43 @@
 #include <stdio.h>
-int main() {  
+int main() {
+    {}
+    {}
+    {}
+    {}
     FILE *inp;
     FILE *outp;
 
-    char array_counter;
-    char text[1024];
+    char text[512];
     char index = &text;    
-    int key=25;
+    inp = fopen("Caesar Input", "r");
+    outp = fopen("Caesar Decryption", "w");
+    fscanf(inp, "%c", &index);
+    int key = index-22;         //Enter cipher key in the 1st text space of enc.txt
+    fscanf(inp, "%c", &index);  //This removes the cipher key from dec.out and keeps it uniform to stdout
 
-    inp = fopen("enc.txt", "r");
-    outp = fopen("dec.txt", "w");
-    array_counter = fscanf(inp, "%c", &index);
-
-
-    while (feof(inp)==0) {      //while the text isn't at the end of the file
+    while (feof(inp)==0) {      //while (there is still text to read from enc.txt) 
         
         if (65<=index && index<=90) {
-            index = (index - 13 + key) % 26 + 65;
+            index = (index - 13 - key) % 26 + 65;
             fprintf(outp, "%c", index);
             printf("%c", index);        //"All output should be sent to BOTH the file and stdout"- 3.1.2 Outputs
-            array_counter = fscanf(inp, "%c", &index);
+            fscanf(inp, "%c", &index);
         }
         
         else if (97<=index && index<=122) {//"If a lowercase letter is found... convert it to uppercase" - 3.2 Message text specification
-            index = (index - 19 + key) % 26 + 65;
+            index = (index - 19 - key) % 26 + 65;
             fprintf(outp, "%c", index);
             printf("%c", index);        //"All output should be sent to BOTH the file and stdout"- 3.1.2 Outputs
-            array_counter = fscanf(inp, "%c", &index);
+            fscanf(inp, "%c", &index);
         }
         
         else {//"Do not encrypt white space, punctuation or numerals" - 3.2 Message text specification
             fprintf(outp, "%c", index);
             printf("%c", index);        //"All output should be sent to BOTH the file and stdout"- 3.1.2 Outputs
-            array_counter = fscanf(inp, "%c", &index);
+            fscanf(inp, "%c", &index);
         }  
-        
+    
+
     }
     
     fclose(inp);
